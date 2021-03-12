d4445bbd4d0e8b81889982c237c78c933bd8b363
Górka Mateusz <maatiug@gmail.com>
2021-03-12 21:51:57 +0100

    Opis funkcji algorytmu

--- Labolatorium 1/labolatorium1/all_possibilities.py
+++ Labolatorium 1/labolatorium1/all_possibilities.py
@@ -1,9 +1,22 @@
 from labolatorium1.general_lib import *
 
 class AllPosibilites:
-    def run(self, machines: list[Machine], tasks: list[Task]) -> list[Machine]:
+    def run(self, machines: list, tasks: list) -> list:
+        """ Sprawdza wszystkie możliwości
+
+        Parameters
+        ----------
+        machines : list
+            Lista maszyn z ustawionymi czasami zadań;
+        tasks : list
+            Lista zadań;
+
+        Returns
+        -------
+        list
+            Lista maszyn z dodanymi zadaniami do listy;
+        """
 
         # todo
 
         pass
-    
