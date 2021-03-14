a8036b206ab2911fffd3b563b947b78de33f758c
KowalDrzo <61516230+KowalDrzo@users.noreply.github.com>
2021-03-14 20:50:46 +0100

    Algorytm działa, mam nadzieję, że dobrze :D

--- Labolatorium 1/labolatorium1/johnson_rule.py
+++ Labolatorium 1/labolatorium1/johnson_rule.py
@@ -1,6 +1,23 @@
 from labolatorium1.general_lib import *
 
 class JohnsonRule:
+
+    def taskWminDuration(self, machines: list, tasks: list) -> Task:
+
+        tmpTsk = tasks[0]
+        min = 9999
+        for task in tasks:
+            for machine in machines:
+                if machine.get_task_duration(task) < min:
+                    min = machine.get_task_duration(task)
+                    tmpTsk = task
+
+        for task in tasks:
+            if task is tmpTsk:
+                tasks.remove(task)
+        
+        return tmpTsk
+
     def run(self, machines: list, tasks: list) -> list:
         """ Algorytm Johnsona
 
@@ -17,6 +34,27 @@ class JohnsonRule:
             Lista maszyn z dodanymi zadaniami do listy;
         """
 
-        # todo
+        beginInd = 0
+        endInd = len(tasks)
 
-        pass
+        newTasks = [None] * endInd
+
+        while beginInd < endInd:
+            
+            minTask = self.taskWminDuration(machines, tasks)
+
+            machineA = machines[0]
+
+            if machineA.has_task(minTask):
+                newTasks[0] = minTask
+                beginInd += 1
+            else:
+                newTasks[endInd - 1] = minTask
+                endInd -= 1
+
+
+        for task in newTasks:
+            for machine in machines:
+                machine.add_task(task)
+
+        return machines
