bcbb7d21961d6628a04ef95ab7aed4c355ab7473
Górka Mateusz <maatiug@gmail.com>
2021-04-03 14:51:21 +0200

    Usuniecie blednych testow

--- Labolatorium_1_i_2/tests/test_NEH_modifications.py
+++ Labolatorium_1_i_2/tests/test_NEH_modifications.py
@@ -15,43 +15,4 @@ def test_without_modification_1():
     file_name = f"{dirname}/../example_data/data.001"
     neh_file = NehFileReader(file_name)
     neh_file.verify_algorithm(NehAlgorithmModification(WithoutModification()))
-
-def test_first_modification():
-    file_name = f"{dirname}/../example_data/data.002"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(FirstModification()))
-
-def test_first_modification():
-    file_name = f"{dirname}/../example_data/data.001"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(FirstModification()))
-
-def test_sec_modification():
-    file_name = f"{dirname}/../example_data/data.002"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(SecondModification()))
-
-def test_sec_modification():
-    file_name = f"{dirname}/../example_data/data.001"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(SecondModification()))
-
-def test_th_modification():
-    file_name = f"{dirname}/../example_data/data.002"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(ThirdModification()))
-
-def test_th_modification():
-    file_name = f"{dirname}/../example_data/data.001"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(ThirdModification()))
-
-def test_fo_modification():
-    file_name = f"{dirname}/../example_data/data.002"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(FourthModification()))
-
-def test_foth_modification():
-    file_name = f"{dirname}/../example_data/data.001"
-    neh_file = NehFileReader(file_name)
-    neh_file.verify_algorithm(NehAlgorithmModification(FourthModification()))
+    
