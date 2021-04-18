91e14916abeaf92ed27a0e3c1d66d05a0bbbeaa4
Górka Mateusz <maatiug@gmail.com>
2021-04-18 22:55:04 +0200

    styslistka w code review

--- flow_shop_scheduling/laboratorium3/neightbourhood_generator.py
+++ flow_shop_scheduling/laboratorium3/neightbourhood_generator.py
@@ -8,7 +8,7 @@ Solution = List[Task]
 
 class NeightbourhoodGenerator:
     name = ""
-    
+
     def run(self, current_solution: Solution) -> Iterator[Solution]:
         raise NotImplementedError()
 
@@ -34,15 +34,13 @@ class Insert(NeightbourhoodGenerator):
             temp = solution_copy[first_index]
 
             if first_index < second_index:
-                tmp_index = first_index
-                first_index = second_index
-                second_index = tmp_index
-            
+                first_index, second_index = second_index, first_index
+
             for i in range(first_index, second_index - 1):
                 solution_copy[i] = solution_copy[i + 1]
 
             solution_copy[second_index] = temp
-            
+
             yield solution_copy
 
 
@@ -57,20 +55,18 @@ class Inverse(NeightbourhoodGenerator):
         temp = solution_copy[first_index]
         solution_copy[first_index] = solution_copy[second_index]
         solution_copy[second_index] = temp
-        
+
         self.swapRecursive(first_index + 1, second_index - 1, solution_copy)
 
     def run(self, solution: Solution) -> Iterator[Solution]:
-        
+
         for first_index, second_index in itertools.combinations(range(len(solution)), 2):
-            
+
             solution_copy = solution.copy()
 
             if first_index < second_index:
-                tmp_index = first_index
-                first_index = second_index
-                second_index = tmp_index
-            
+                first_index, second_index = second_index, first_index
+
             self.swapRecursive(first_index, second_index, solution_copy)
 
             yield solution_copy
