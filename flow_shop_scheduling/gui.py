91597f8e25e1ca827eebf6c75c8e992eba502341
KowalDrzo <61516230+KowalDrzo@users.noreply.github.com>
2021-04-19 00:16:54 +0200

    Popoprawiam rano

--- flow_shop_scheduling/gui.py
+++ flow_shop_scheduling/gui.py
@@ -7,10 +7,10 @@ class Gui:
 
     command = []
 
-    var_s = tk.IntVar(0)
-    var_p = tk.IntVar(0)
-    var_j = tk.IntVar(0)
-    var_n = tk.IntVar(0)
+    var_s = None
+    var_p = None
+    var_j = None
+    var_n = None
 
 
     def button_interrupt(self):
@@ -27,11 +27,15 @@ class Gui:
         if self.var_n.get() == 1:
             self.command.append("-n")        
 
+        print(self.command)
         subprocess.run(self.command)
         exit() # opcjonalnie
 
 
     def __init__(self):
+
+        root = tk.Tk()
+        root.withdraw()
 
         self.command.append("python3")
         self.command.append("__main__.py")
@@ -44,11 +48,16 @@ class Gui:
         window.title("Wybór algorytmu")
         window.geometry("200x200")
 
+        self.var_s = tk.IntVar()
+        self.var_p = tk.IntVar()
+        self.var_j = tk.IntVar()
+        self.var_n = tk.IntVar()
 
-        s_box = tk.Checkbutton(window,  text="Kolejność domyślna",  variable = self.var_s, onvalue = 1, offvalue = 0)
-        p_box = tk.Checkbutton(window,  text="Przegląd zupełny",    variable = self.var_p, onvalue = 1, offvalue = 0)
-        j_box = tk.Checkbutton(window,  text="Reguła Johnsona",     variable = self.var_j, onvalue = 1, offvalue = 0)
-        n_box = tk.Checkbutton(window,  text="Algorytm Neh",        variable = self.var_n, onvalue = 1, offvalue = 0)
+
+        s_box = tk.Checkbutton(window,  text="Kolejność domyślna",  variable = self.var_s)
+        p_box = tk.Checkbutton(window,  text="Przegląd zupełny",    variable = self.var_p)
+        j_box = tk.Checkbutton(window,  text="Reguła Johnsona",     variable = self.var_j)
+        n_box = tk.Checkbutton(window,  text="Algorytm Neh",        variable = self.var_n)
         button = tk.Button(window,      text ="Uruchom", command = self.button_interrupt)
 
 
