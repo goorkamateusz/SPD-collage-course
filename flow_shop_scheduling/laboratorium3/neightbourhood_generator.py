aa4ad9fa6cc3bb1df41548a0ca12161444d58b63
KowalDrzo <61516230+KowalDrzo@users.noreply.github.com>
2021-04-18 22:42:44 +0200

    Inverse

--- flow_shop_scheduling/laboratorium3/neightbourhood_generator.py
+++ flow_shop_scheduling/laboratorium3/neightbourhood_generator.py
@@ -43,12 +43,34 @@ class Insert(NeightbourhoodGenerator):
 
             solution_copy[second_index] = temp
             
-            solution_copy[first_index], solution_copy[second_index] = solution_copy[second_index], solution_copy[first_index]
             yield solution_copy
 
 
 class Inverse(NeightbourhoodGenerator):
     name = "Inverse"
 
+    def swapRecursive(self, first_index, second_index, solution_copy: Solution):
+
+        if first_index >= second_index:
+            return
+
+        temp = solution_copy[first_index]
+        solution_copy[first_index] = solution_copy[second_index]
+        solution_copy[second_index] = temp
+        
+        self.swapRecursive(first_index + 1, second_index - 1, solution_copy)
+
     def run(self, solution: Solution) -> Iterator[Solution]:
-        raise NotImplementedError()
+        
+        for first_index, second_index in itertools.combinations(range(len(solution)), 2):
+            
+            solution_copy = solution.copy()
+
+            if first_index < second_index:
+                tmp_index = first_index
+                first_index = second_index
+                second_index = tmp_index
+            
+            self.swapRecursive(first_index, second_index, solution_copy)
+
+            yield solution_copy
