ceba2e6ff3cf0f77a320a22fa307e07b89d2f611
KowalDrzo <248959@student.pwr.edu.pl>
2021-05-05 12:33:13 +0200

    poprawki

--- rpq_problem/Mkh_splitter/randomizer.py
+++ rpq_problem/Mkh_splitter/randomizer.py
@@ -8,11 +8,11 @@ for nb in examples_nb:
 
     file = open("../example_data/rand/rand." + str(nb), "w")
 
-    file.writelines(str(nb))
+    file.writelines(str(nb) + "\n")
 
     line = ""
     for i in range(0, nb):
-        line = str(random.randint(0, max_t)) + " " + str(random.randint(0, max_t)) + " " + str(random.randint(0, max_t))
+        line = str(random.randint(0, max_t)) + " " + str(random.randint(0, max_t)) + " " + str(random.randint(0, max_t)) + "\n"
         file.writelines(line)
 
     file.close()
