0f0138b2ad7e686747da1d5e64246d573b4ef063
Górka Mateusz <maatiug@gmail.com>
2021-05-25 21:43:25 +0200

    Moja proba (tez nie dziala)

--- rpq_problem/laboratorium5/calier_algorithm.py
+++ rpq_problem/laboratorium5/calier_algorithm.py
@@ -1,3 +1,4 @@
+import copy
 from laboratorium4.Cmax_calculator import CMaxCalculator
 from typing import List, Optional
 import sys
@@ -53,7 +54,8 @@ class CarlierAlgorithm(Algorithm):
         
         task_c_copy = task_c.copy()
         task_c.change_preparation_time(max(task_c.get_preparation_time(), r_K + p_K))
-       
+        # tasks[tasks.index(task_c)] = task_c
+
         lower_bound = self.schragePMTN.run(tasks)
         lower_bound = max(self.count_h_K(list_K), self.count_h_K([task_c] + list_K), lower_bound)
 
@@ -61,24 +63,25 @@ class CarlierAlgorithm(Algorithm):
             #tasks = self.carlier(tasks, upper_bound)
             self.permutations.append([tasks, upper_bound])
             return tasks
-        
+
         #################################
-       
-        # task_c = task_c_copy.copy()
+
         task_c.change_preparation_time(task_c_copy.get_preparation_time())
 
         task_c.change_delivery_time(max(task_c.get_delivery_time(), q_K + p_K))
+        # tasks[tasks.index(task_c)] = task_c
 
         lower_bound = self.schragePMTN.run(tasks)
         lower_bound = max(self.count_h_K(list_K), self.count_h_K([task_c] + list_K), lower_bound)
-       
+
         if lower_bound < upper_bound:
             #tasks = self.carlier(tasks, upper_bound)
-            self.permutations.append([tasks, upper_bound])
+            self.permutations.append([copy.deepcopy(tasks), upper_bound])
             return tasks
 
         # task_c = task_c_copy.copy()
         task_c.change_delivery_time(task_c_copy.get_delivery_time())
+        # tasks[tasks.index(task_c)] = task_c
 
         return tasks
 
@@ -86,53 +89,45 @@ class CarlierAlgorithm(Algorithm):
     #TODO!!! test
 
     def count_task_b(self, tasks: List[Task]) -> Task:
-        offset = 10
-
-        temp_task = None
         cmax = self.cmax_calc.get_Cmax(tasks)
 
         for task in tasks:
-            tmp_q = task.get_delivery_time()
-            task.change_delivery_time(tmp_q+offset)
-            
-            if cmax + offset == self.cmax_calc.get_Cmax(tasks):
-                temp_task = task
-            task.change_delivery_time(tmp_q)
+            id = tasks.index(task)
+            c = self.cmax_calc.get_Cmax(tasks[0 : id+1])
+            if c == cmax:
+                return task
 
-        if temp_task == None:
-            raise ValueError
-        return temp_task
+        raise NotImplementedError()
 
     #######################################################################
     #TODO!!! do
 
     def count_task_a(self, tasks: List[Task], task_b: Task) -> Task:
-       
-        task_a = tasks[0]
-        sum = task_a.get_preparation_time() + task_a.get_execution_time()
+        timeline = dict()
+        t = 0
 
-        for i in range(1, len(tasks)):
-            if sum < tasks[i].get_preparation_time():
-                task_a = tasks[i]
+        for task in tasks:
+            t = max(t, task.get_preparation_time())
+            t += task.get_execution_time()
+            timeline[task] = t
 
-            sum += tasks[i].get_preparation_time() + tasks[i].get_execution_time()
-            if tasks[i] == task_b:
+        last_correct = task_b
+
+        for task in reversed(tasks[0 : tasks.index(task_b)]):
+            if timeline[task] != timeline[last_correct] - last_correct.get_execution_time():
                 break
+            last_correct = task
 
-        return task_a
+        return last_correct
 
     #######################################################################
     #TODO!!! test
 
     def count_task_c(self, tasks: List[Task], task_a: Task, task_b: Task) -> Optional[Task]:
-        sublist = tasks[tasks.index(task_a):tasks.index(task_b) +1]
-        temp_task = None
-
-        for task in sublist:
+        for task in reversed(tasks[tasks.index(task_a) : tasks.index(task_b) + 1]):
             if task.get_delivery_time() < task_b.get_delivery_time():
-                temp_task = task
-
-        return temp_task
+                return task
+        return None
 
     #######################################################################
     #TODO!!! test
@@ -148,7 +143,7 @@ class CarlierAlgorithm(Algorithm):
     #TODO!!! test
 
     def fill_list_k(self, tasks: List[Task], task_c: Task, task_b: Task) -> List[Task]:
-        return tasks[tasks.index(task_c) + 1:tasks.index(task_b) + 1]
+        return tasks[tasks.index(task_c) + 1 : tasks.index(task_b) + 1]
 
     #######################################################################
     #######################################################################
@@ -156,8 +151,8 @@ class CarlierAlgorithm(Algorithm):
     #TODO!!! test
 
     def run(self, tasks: List[Task]) -> List[Task]:
-        #sys.setrecursionlimit(99999)
-        #return self.carlier(tasks)
+        # sys.setrecursionlimit(99999)
+        # return self.carlier(tasks)
 
         temp_tasks = self.carlier(tasks)
         best_tasks = temp_tasks.copy()
