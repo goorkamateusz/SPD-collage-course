8cded79e6ce8505f16d6b7109182ff5eb9fcaec8
KarolinaGl <karolina.gluszek6@gmail.com>
2021-06-07 19:06:01 +0200

    Optymalizacja problemu flowshop w solverze

--- flow_shop_scheduling/laboratorium6/ortools_flowshop.py
+++ flow_shop_scheduling/laboratorium6/ortools_flowshop.py
@@ -26,9 +26,27 @@ class FlowshopSolver(Algorithm):
         for machine in machines:
             for task in tasks:
                 variable_max_value += machine.get_task_duration(task)
-        print(variable_max_value)
 
-        cmax_optimalization_objective = model.NewIntVar(variable_min_value, variable_max_value, 'cmax_makespan')
+        min_values = []
+        first_row = []
+        for i in range(0, len(tasks)):
+            first_row.append(0)
+        min_values.append(first_row)
+
+        for machine_index in range(len(machines)-1):
+            machine_row = []
+            for task in tasks:
+                machine_row.append(machines[machine_index].get_task_duration(task) + min_values[machine_index][task.get_id()-1])
+            min_values.append(machine_row)
+
+        max_values = []
+        machine_sum = 0
+        for machine_index, machine in enumerate(machines):
+            for task in tasks:
+                machine_sum += machine.get_task_duration(task)
+            max_values.append(machine_sum)
+
+        cmax_optimization_objective = model.NewIntVar(variable_min_value, variable_max_value, 'cmax_makespan')
 
         model_start_vars = []
         model_ends_vars = []
@@ -39,9 +57,9 @@ class FlowshopSolver(Algorithm):
             start_vars = []
             for task in tasks:
                 suffix = f"t:{task.get_id()}_{machine.get_id()}"
-                start_var = model.NewIntVar(variable_min_value, variable_max_value, 'start_' + suffix)
+                start_var = model.NewIntVar(min_values[machine.get_id()-1][task.get_id()-1], variable_max_value, 'start_' + suffix)
                 start_vars.append(start_var)
-                end_var = model.NewIntVar(variable_min_value, variable_max_value, 'end_' + suffix)
+                end_var = model.NewIntVar(variable_min_value, max_values[machine.get_id()-1], 'end_' + suffix)
                 end_vars.append(end_var)
                 interval_var = model.NewIntervalVar(start_var, machine.get_task_duration(task), end_var, 'interval_' + suffix)
                 model_interval_vars.append(interval_var)
@@ -49,11 +67,8 @@ class FlowshopSolver(Algorithm):
             model_start_vars.append(start_vars)
             model.AddNoOverlap(model_interval_vars)
 
-        # print(model_start_vars)
-        # print(model_ends_vars)
-
         for end_var in model_ends_vars[-1]:
-            model.Add(cmax_optimalization_objective >= end_var)
+            model.Add(cmax_optimization_objective >= end_var)
 
         for i in range(len(machines)-1):
             for j in range(len(tasks)):
@@ -63,15 +78,15 @@ class FlowshopSolver(Algorithm):
             for first_index, second_index in combinations(range(len(tasks)), 2):
                 suffix = f"t:{machine.get_id()}_{first_index}_{second_index}"
                 is_first_greater_than_second = model.NewBoolVar('is_positive' + suffix)
-                model.Add((model_start_vars[i][second_index] - model_start_vars[i][first_index]) >= -variable_max_value*(1-is_first_greater_than_second))
-                model.Add((model_start_vars[i+1][second_index] - model_start_vars[i+1][first_index]) >= -variable_max_value*(1-is_first_greater_than_second))
-                model.Add((model_start_vars[i][second_index] - model_start_vars[i][first_index]) <= variable_max_value*is_first_greater_than_second)
-                model.Add((model_start_vars[i+1][second_index] - model_start_vars[i+1][first_index]) <= variable_max_value*is_first_greater_than_second)
+                model.Add((model_start_vars[i][second_index] - model_start_vars[i][first_index]) >= -max_values[i]*(1-is_first_greater_than_second))
+                model.Add((model_start_vars[i+1][second_index] - model_start_vars[i+1][first_index]) >= -max_values[i+1]*(1-is_first_greater_than_second))
+                model.Add((model_start_vars[i][second_index] - model_start_vars[i][first_index]) <= max_values[i]*is_first_greater_than_second)
+                model.Add((model_start_vars[i+1][second_index] - model_start_vars[i+1][first_index]) <= max_values[i+1]*is_first_greater_than_second)
 
-        model.Minimize(cmax_optimalization_objective)
+        model.Minimize(cmax_optimization_objective)
 
         solver = cp_model.CpSolver()
-        solver.parameters.max_time_in_seconds = 60.0
+        # solver.parameters.max_time_in_seconds = 60.0
         status = solver.Solve(model)
 
         if status is not cp_model.OPTIMAL:
@@ -79,12 +94,6 @@ class FlowshopSolver(Algorithm):
         else:
             status_readable = "optimum found!"
 
-        print(status, status_readable)
-
         pi_order = sorted(range(len(tasks)), key=lambda task_number: solver.Value(model_start_vars[0][task_number]))
-
-        for i in range(len(machines)):
-            pi_order_ = sorted(range(len(tasks)), key=lambda task_number: solver.Value(model_start_vars[i][task_number]))
-            print(pi_order_)
 
         return solver.ObjectiveValue(), pi_order, status_readable
