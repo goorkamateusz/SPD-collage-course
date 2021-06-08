a5e59a5dd8db302982f59a2221e94d2809f2038e
KowalDrzo <248959@student.pwr.edu.pl>
2021-06-08 23:58:40 +0200

    Resztę ogarniam rano

--- job_shop/__main__.py
+++ job_shop/__main__.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+import job_shop
+
+def job_shop_ortools(file_path):
+    test_instance = job_shop.load_from_file(file_path)
+    cmax, pi_order, status = job_shop.job_shop_ortools(test_instance)
+    print(f"Cmax: {cmax}\norder: {pi_order}\n{status}")
 
 if __name__ == "__main__":
-    pass
+    
+    job_shop_ortools("Mkh_splitter/insa.data.txt")
